7cd40d9eae809f89c93cf024a1a6b90e276d368e
Dende <alegrria@gmail.com>
2016-12-05 15:54:54 +0100

    Filters improved

--- urls.py
+++ urls.py
@@ -4,4 +4,8 @@ from test_mlj import views
 
 urlpatterns = [
                url(r'^$', views.index, name='index'),
+               url(r'^album$', views.album, name='album'),
+               url(r'^artist$', views.artist, name='artist'),
+               url(r'^playlist$', views.playlist, name='playlist'),
+               url(r'^track$', views.track, name='track'),
 ]
--- views.py
+++ views.py
@@ -10,6 +10,40 @@ def index(request):
     list_of_songs.append(song1)
     list_of_songs.append(song2)
     context = {'list_of_songs': list_of_songs, 'length': len(list_of_songs)}
-    
     return render(request, 'test_mlj/index.html', context)
 
+def album(request):
+    song1 = Song('song1', 'pic1')
+    song2 = Song('song2', 'pic2')
+    list_of_songs = []
+    list_of_songs.append(song1)
+    list_of_songs.append(song2)
+    context = {'list_of_songs': list_of_songs, 'length': len(list_of_songs)}
+    return render(request, 'test_mlj/index.html', context)
+
+def artist(request):
+    song1 = Song('song1', 'pic1')
+    song2 = Song('song2', 'pic2')
+    list_of_songs = []
+    list_of_songs.append(song1)
+    list_of_songs.append(song2)
+    context = {'list_of_songs': list_of_songs, 'length': len(list_of_songs)}
+    return render(request, 'test_mlj/index.html', context)
+
+def playlist(request):
+    song1 = Song('song1', 'pic1')
+    song2 = Song('song2', 'pic2')
+    list_of_songs = []
+    list_of_songs.append(song1)
+    list_of_songs.append(song2)
+    context = {'list_of_songs': list_of_songs, 'length': len(list_of_songs)}
+    return render(request, 'test_mlj/index.html', context)
+
+def track(request):
+    song1 = Song('song1', 'pic1')
+    song2 = Song('song2', 'pic2')
+    list_of_songs = []
+    list_of_songs.append(song1)
+    list_of_songs.append(song2)
+    context = {'list_of_songs': list_of_songs, 'length': len(list_of_songs)}
+    return render(request, 'test_mlj/index.html', context)
