d46572bd104d8146e6df08a233119d1d74246c49
Dende <alegrria@gmail.com>
2016-12-05 15:04:00 +0100

    Plugs for songs added

--- views.py
+++ views.py
@@ -7,7 +7,9 @@ def index(request):
     song1 = Song('song1', 'pic1')
     song2 = Song('song2', 'pic2')
     list_of_songs = []
-    list_of_songs.append(song1.name)
-    list_of_songs.append(song2.name)
-    return render(request, 'test_mlj/index.html', {})
+    list_of_songs.append(song1)
+    list_of_songs.append(song2)
+    context = {'list_of_songs': list_of_songs}
+    
+    return render(request, 'test_mlj/index.html', context)
 
